0710f99ca21955997ee329d3009eb989f69ee714
Nattural20 <meernatkat@gmail.com>
2025-02-03 21:37:25 +1100

    I THINK I NETWORKD

---
+++ sabby-boshi/Assets/RelayManager.cs
@@ -0,0 +1,63 @@
+using System.Threading.Tasks;
+using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+using Unity.Services.Relay;
+using Unity.Netcode;
+using Unity.Services.Relay.Models;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine.SceneManagement;
+
+public class RelayManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI joinCodeText;
+    [SerializeField] private TMP_InputField joinCodeInputField;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    public async void StartRelay() 
+    {
+        string joinCode = await StartHostWithRelay();
+        joinCodeText.text = joinCode;
+        
+    }
+
+    public async void JoinRelay() 
+    {
+        await StartClientWithRelay(joinCodeInputField.text);
+    }
+    
+    private async Task<string> StartHostWithRelay(int maxConnections = 3) 
+    {
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, "dtls"));
+
+        string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+        return NetworkManager.Singleton.StartHost() ? joinCode : null;
+    }
+
+    private async Task<bool> StartClientWithRelay(string joinCode) 
+    {
+        JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(joinAllocation, "dtls"));
+
+        return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient();
+
+        //NetworkManager.Singleton.SceneManager.LoadScene("Nats Scene")
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
